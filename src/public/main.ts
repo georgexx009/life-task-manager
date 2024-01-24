4c98fff54edb5626f2742853c22bd03a63a487fd
Jorge Galindo <jorge.galindo@wizeline.com>
2024-01-23 18:00:37 -0600

    base set up build and start

--- src/public/main.ts
+++ src/public/main.ts
@@ -1,1 +1,2 @@
-console.log('hello from console')
+console.log('hello from console2')
+console.log('hello from console2')
--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,18 @@ import path from 'path'
 const app = express();
 const port = 3000;
 
-// TODO - MAKE THIS ENV VAR TO CAN RUN IT IN DEV TOO
-app.use(express.static(path.join(__dirname, '..', 'dist/public')))
+if (process.env.NODE_ENV === 'development') {
+  app.use('/@vite/client', express.static(path.join(__dirname, '../node_modules/@vite/client/dist')))
+}
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'dist/public', 'index.html'))
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});
+
+app.get('/two', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index-2.html'))
 });
 
 app.listen(port, () => {
